1bbbc73bd657e29dbe0edcf2e3ec5ea37fd5bdca
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2021-11-22 23:06:58 -0700

    Bold title

--- main.sh
+++ main.sh
@@ -1,5 +1,7 @@
+bold=$(tput bold);
+normal=$(tput sgr0);
 FTemp=$(vcgencmd measure_temp |tr -cd '[[:digit:]].');
-echo $'Temperature: \n';
+echo "${bold}Temperature:${normal}";
 echo "Celsius: ${FTemp}";
 echo "Kelvin: $(echo "${FTemp} + 273" | bc)";
 echo "Fahrenheit: $(echo "1.8*${FTemp}+32" | bc)"
