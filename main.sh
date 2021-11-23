c0972bf83c7f1a2bf3cbc81711a04a629fc15075
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2021-11-23 16:03:49 -0700

    Newlines to stand out

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 # Colors
-WHT=$(tput setaf 15);
+WHT=$(tput setaf 7);
 RED=$(tput setaf 1);
 GRN=$(tput setaf 2);
 
@@ -10,10 +10,10 @@ normal=$(tput sgr0);
 
 
 # Get current temperature
-FTemp=$(vcgencmd measure_temp | tr -cd '[[:digit:]].');
+FTemp=$(vcgencmd measure_temp |tr -cd '[[:digit:]].');
 
 # Display temperature in respective units
-echo "${bold}Temperature:${normal}";
+echo -e "\n${bold}Temperature:${normal}";
 echo "Celsius: ${FTemp}";
 echo "Kelvin: $(echo "${FTemp} + 273" | bc)";
 echo "Fahrenheit: $(echo "1.8*${FTemp}+32" | bc)";
@@ -25,5 +25,5 @@ if [ ${FTemp:0:1} -gt 58 ];
 then
   echo "${stmsg} ${RED}Too hot!";
 else
-  echo "${stmsg} ${GRN}Safe";
+  echo -e "${stmsg} ${GRN}Safe${WHT}\n";
 fi;
