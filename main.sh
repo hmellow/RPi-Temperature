e86666c6dda3d9e6af2bb2a0de78beff431c47f8
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2021-11-22 22:54:06 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,17 +1,5 @@
-# FTemp=$(vcgencmd measure_temp |tr -cd '[[:digit:]].'); echo "${FTemp}+273" | bc; echo ${FTemp}
-
-
-
-
-
-
 FTemp=$(vcgencmd measure_temp |tr -cd '[[:digit:]].');
 echo $'Temperature: \n';
 echo "Celsius: ${FTemp}";
 echo "Kelvin: $(echo "${FTemp} + 273" | bc)";
 echo "Fahrenheit: $(echo "1.8*${FTemp}+32" | bc)"
-
-
-FTemp=$(vcgencmd measure_temp |tr -cd '[[:digit:]].'); echo $'Temperature: \n'; echo "Celsius: ${FTemp}"; echo "Kelvin: $(echo "${FTemp} + 273" | bc)"; echo "Fahrenheit: $(echo "1.8*${FTemp}+32" | bc)"
-
-
